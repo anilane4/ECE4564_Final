560eceb9a80b84738dcbc7db5529931c9c5d0e97
anilane4 <anilane4@vt.edu>
2014-12-02 14:42:46 -0500

    little fixes 2

--- temp.py
+++ temp.py
@@ -10,13 +10,12 @@ Temp: 'tempuerature in F'
 
 __author__ = 'Martin Anilane'
 
-import pika
-import pika.exceptions
 import sys
 import getopt
 import signal
 import json
 import time
+import socket
 
 # Global variable that controls running the app
 publish_Temp = True
@@ -35,7 +34,7 @@ def getTemperatureData( numInArray):
     mylist = ["75", "74", "79", "70", "70", "70", "71", "72", "75", "68"]
     print "this temp = " + mylist[numInArray]
     #sleep for one sec
-    time.sleep(15)
+    time.sleep(5)
     return mylist[numInArray]
 
 
@@ -43,14 +42,7 @@ def getTemperatureData( numInArray):
 def main(argv):
     try:
         zoneNumber = 0
-        broker = None
-        v_host = '/'
-        login = "guest"
-        password = "guest"
-        routkey = None
-        # Setup signal handlers to shutdown this app when SIGINT or SIGTERM is
-        # sent to this app
-        # For more info about signals, see: https://scholar.vt.edu/portal/site/0a8757e9-4944-4e33-9007-40096ecada02/page/e9189bdb-af39-4cb4-af04-6d263949f5e2?toolstate-701b9d26-5d9a-4273-9019-dbb635311309=%2FdiscussionForum%2Fmessage%2FdfViewMessageDirect%3FforumId%3D94930%26topicId%3D3507269%26messageId%3D2009512
+        host = ""
         signal_num = signal.SIGINT
         try:
             signal.signal(signal_num, stop_temp_service)
@@ -61,7 +53,7 @@ def main(argv):
 
         # get the opts
         try:
-            opts, args = getopt.getopt(argv, "z:")
+            opts, args = getopt.getopt(argv, "z:h:")
         #error handle for the incorrect values
         except getopt.GetoptError:
             print "temp -z zone number"
@@ -77,30 +69,27 @@ def main(argv):
                 else:
                     print "Zone number must be a int"
                     sys.exit(2)
+            elif opt == '-h':
+                host = arg
 
         # check the boolean for the set option
         if zopt == False:
             print "must put in zone number"
             sys.exit(2)
 
-        msg_broker = None
-        ch = None
+        mainSocket = None
+        print "im here"
         try:
-            # connect to the message broker
-            msg_broker = pika.BlockingConnection(
-                pika.ConnectionParameters(host='192.168.1.22',
-                                          virtual_host='environment_host',
-                                          credentials=pika.PlainCredentials('leonp92',
-                                                                            'raspberry',
-                                                                            True)))
-
-            print "Connected to message broker"
-
-            # setup the exchange
-            ch = msg_broker.channel()
-            # try:
-            ch.exchange_declare(exchange="environment_broker",
-                                type="direct")
+            try:
+                mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                mainSocket.connect((str(host), int(2001)))
+            except socket.error, (value, message):
+                print "closing socket: " + message
+                if mainSocket:  
+                    mainSocket.close()
+                sys.exit()
+                
+            print "Connected to host"
             #my dictionary for my information to be sent out
             zone_data = {"Zone": zoneNumber, "Temp": "0"}
             myCount = 0
@@ -112,26 +101,14 @@ def main(argv):
                     myCount = 0
                 json_message = json.dumps(zone_data)
                 # Send the message
-                ch.basic_publish(exchange="environment_broker",
-                            routing_key="zone_info",
-                            body=json_message)
-
-        except pika.exceptions.AMQPError, ae:
-            print "Error: An AMQP Error occured: " + ae.message
-
-        except pika.exceptions.ChannelError, ce:
-            print "Error: A channel error occured: " + ce.message
-
+                mainSocket.send(json_message)
         except Exception, eee:
             print "Error: An unexpected exception occured: " + eee.message
 
         finally:
-            if ch is not None:
-                print "closing channel"
-                ch.close()
-            if msg_broker is not None:
-                print "closing broker"
-                msg_broker.close()
+            if mainSocket is not None:
+                print "closing socket"
+                mainSocket.close()
     except Exception, ee:
         # unkown error shouting down
         print ee
